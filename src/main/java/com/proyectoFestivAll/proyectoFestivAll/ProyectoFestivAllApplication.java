729db95c19f2390bcd3b1ccbc129f22a105283c4
OttonLucena <ottonlucena@gmail.com>
2024-07-14 12:30:42 -0400

    Configurando CORS

--- src/main/java/com/proyectoFestivAll/proyectoFestivAll/ProyectoFestivAllApplication.java
+++ src/main/java/com/proyectoFestivAll/proyectoFestivAll/ProyectoFestivAllApplication.java
@@ -10,24 +10,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class ProyectoFestivAllApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProyectoFestivAllApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoFestivAllApplication.class, args);
+    }
 
-	@Configuration
-	public static class Myconfiguration{
-		@Bean
-		public WebMvcConfigurer corsConfigurer(){
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedOrigins("*")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
-							.allowedHeaders("*");
-				}
-			};
-		}
-	}
+    @Configuration
+    public static class Myconfiguration {
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                }
+            };
+        }
+    }
 
 }
