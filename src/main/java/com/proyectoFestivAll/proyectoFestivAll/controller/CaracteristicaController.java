038f9832d8f957e49899129e70261f06ef22f0ae
OttonLucena <ottonlucena@gmail.com>
2024-07-14 12:36:47 -0400

    Caracteristicas Controller

--- src/main/java/com/proyectoFestivAll/proyectoFestivAll/controller/CaracteristicaController.java
+++ src/main/java/com/proyectoFestivAll/proyectoFestivAll/controller/CaracteristicaController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/caracteristicas")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CaracteristicaController {
 
     @Autowired
